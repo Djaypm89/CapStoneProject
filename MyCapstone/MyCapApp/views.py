dbc0e40f67a33f017a60d23c786dd848d962a2be
Djaypm89 <djaypm89@gmail.com>
2021-10-07 15:08:49 -0400

    APIdetail class add to view file

--- MyCapstone/MyCapApp/views.py
+++ MyCapstone/MyCapApp/views.py
@@ -1,9 +1,7 @@
-
+from django.http.response import Http404
+from rest_framework import serializers
 from .models import HousingData
 from .serializers import HousingDataSerializer
-# from django import APIView
-# from django import Response
-# from django import status
 from rest_framework.views import APIView
 from rest_framework.views import Response
 from rest_framework.views import status
@@ -22,3 +20,30 @@ class HousingDataList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class HousingDataDetail(APIView):
+
+    def get_object(self,pk):
+        try:
+            return HousingData.objects.get(pk=pk)
+        except HousingData.DoesNotExist:
+            raise Http404
+
+    def get(self,request,pk):
+        housingdata = self.get_object(pk)
+        serializer = HousingDataSerializer(housingdata)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        housingdata = self.get_object(pk)
+        serializer = HousingDataSerializer(housingdata, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        housingdata = self.get_object(pk)
+        housingdata.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
