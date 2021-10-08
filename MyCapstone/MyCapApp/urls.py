7966f6d6e3384dd03bf169df61ff9ac2cfd00d18
Djaypm89 <djaypm89@gmail.com>
2021-10-07 22:50:22 -0400

    just in case push

--- MyCapstone/MyCapApp/urls.py
+++ MyCapstone/MyCapApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views 
 
 
-urlpatterns = [path('MyCapApp/', views.HousingDataList.as_view()),
+urlpatterns = [
+    path('MyCapApp/', views.HousingDataList.as_view()),
+    path('MyCapApp/<int:pk>/', views.HousingDataDetail.as_view()),
 ]
+
