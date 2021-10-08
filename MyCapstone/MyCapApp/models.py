dcf13fd5b2cc5a2aa18fbe4257d33df20f36d34f
Djaypm89 <djaypm89@gmail.com>
2021-10-08 15:23:19 -0400

    imported excel db to mysql, updated fields in models and serializers

--- MyCapstone/MyCapApp/models.py
+++ MyCapstone/MyCapApp/models.py
@@ -2,13 +2,17 @@ from django.db import models
 
 # Create your models here.
 class HousingData(models.Model):
+    sizeRank = models.IntegerField(max_length=50)
     country = models.TextField(max_length=50)
     state = models.TextField(max_length=50)
     city = models.TextField(max_length=50)
-    rent = models.IntegerField(max_length=50)
-    price = models.IntegerField(max_length=50)
+    rent1YearAvg = models.IntegerField(max_length=50)
+    rent5YearAvg = models.IntegerField(max_length=50)
+    value1YearAvg = models.IntegerField(max_length=50)
+    value5YearAvg = models.IntegerField(max_length=50)
+    
     # // change below so form can accept floats or remove and have ratio not stored in DB, but displayed thru a function? //
-    ratio = models.FloatField(max_length=50) 
+    # ratio = models.FloatField(max_length=50) 
     
 
 
--- MyCapstone/MyCapApp/serializers.py
+++ MyCapstone/MyCapApp/serializers.py
@@ -6,5 +6,7 @@ from .models import HousingData
 class HousingDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = HousingData
-        fields = ['id', 'country', 'state', 'city', 'rent', 'price', 'ratio']
+        fields = ['id', 'sizeRank' 'country', 'state', 'city', 
+        'rent1YearAvg', 'rent5YearAvg', 'value1YearAvg','value5YearAvg',
+        'price', 'ratio']
         
